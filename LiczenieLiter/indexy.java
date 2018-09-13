ddaad8785ad773ae048714d7de6f96398263d131
xlikes <33297520+xlikes@users.noreply.github.com>
2018-09-13 17:44:19 +0200

    0.1

--- LiczenieLiter/indexy.java
+++
@@ -1,9 +0,0 @@
-public class indexy {
-    public static void index(String slowo){
-        int index1 = slowo.charAt(0);
-        int index2 = slowo.charAt(4);
-        System.out.println("pierwsza litera to "+(char)index1);
-        System.out.println("piata litera to "+(char)index2);
-    }
-}
-
